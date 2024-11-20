741ec77d17c04612710c00dc8993a74e458c968b
Tatsuya Kudo <q2313009@gmail.com>
2024-11-20 19:52:42 +0900

    Initial commit

---
+++ my-app/app/Models/News.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use GuzzleHttp\Client;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class News extends Model
+{
+    use HasFactory;
+
+    //ニュースの取得
+    public static function fetchNews($keyword = null, $pageSize = 5, $page = 1)
+    {
+        $url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' . config('services.newsapi.key');
+
+        //キーワードがある場合はキーワードを含むニュースを取得
+        // 記事数とページ数を指定
+        $url .= '&pageSize=' . $pageSize . '&page=' . $page;
+
+        if ($keyword) {
+            $url .= '&q=' . urlencode($keyword) . '&pageSize=5';
+        }
+        // dd($url);
+        $response = Http::get($url);
+
+        if ($response->failed()) {
+            throw new \Exception('Failed to fetch news data');
+        }
+        return $response->json();
+    }
+
+    public static function summarizeArticle($content)
+    {
+        $client = new Client();
+        $apiKey = config('services.openai.key');
+
+        $systemRole = 'あなたは優秀なWebライターです';
+        $prompt = '以下の記事を100文字程度で要約してください。#記事 /\n' . $content;
+        try {
+            $response = $client->post('https://api.openai.com/v1/chat/completions', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer ' . $apiKey,
+                ],
+                'json' => [
+                    'model' => 'gpt-3.5-turbo',
+                    'messages' => [
+                        ['role' => 'system', 'content' => $systemRole],
+                        ['role' => 'user', 'content' => $prompt],
+                    ],
+                    'max_tokens' => 300,
+                ],
+            ]);
+            $response = json_decode($response->getBody(), true);
+            $summarizedContent =  $response['choices'][0]['message']['content'];
+            return response()->json($summarizedContent);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
---
+++ my-app/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\NewsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// ニュースApp
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+
+Route::get('/', function () {
+    return view('welcome');
+});
